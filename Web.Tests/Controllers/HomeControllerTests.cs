fd8f325b5dd253450232a2b5ee65f45019d600e2
mindaugas <mindaugas.staselis@gmail.com>
2015-03-14 16:49:54 +0200

    Simple test for dummy indes action

---
+++ Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Web.Controllers;
+using System.Web.Mvc;
+
+namespace Web.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void IndexIsNotNull()
+        {
+           
+            HomeController controller = new HomeController();
+
+            
+            ViewResult result = controller.Index() as ViewResult;
+
+           
+            Assert.IsNotNull(result);
+        }
+    }
+}
